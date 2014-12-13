0dcf64802c1343f7d7e0d8d53a516bc6935466ff
Vitaly Lipatov <lav@etersoft.ru>
2014-12-13 17:26:03 +0300

    rewrite to support various length commands

--- tests/test_giter.sh
+++ tests/test_giter.sh
@@ -2,9 +2,14 @@
 
 giter()
 {
+    echo
+    echo "$@: "
+    #shift
     ../bin/giter "$@"
 }
 
 giter list branches
-giter print name
-giter print girar
+giter print repo name
+giter print girar host
+giter list remote
+giter print girar repo lyx
